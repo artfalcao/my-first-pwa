c609016afc31fa988a67895b4b22d8b2e48dced2
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2023-03-17 09:50:49 -0300

    First commit

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+## Hello there! This is my first PWA application.
+
+I use the template bellow to help me to create a page more easily.
+<br>
+Template Name: Laura
+Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ forms/contact.js
@@ -0,0 +1,10 @@
+const nome = document.querySelector("#name").value;
+const subject = document.querySelector("#subject").value;
+const email = document.querySelector("#email").value;
+const message = document.querySelector("#message").value;
+
+function sendWhatsAppMessage() {
+  console.log(nome, subject, email, message)
+}
+
+
