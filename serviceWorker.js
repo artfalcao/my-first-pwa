974243a75d83b234bb6246c1ac058cab32ac9d25
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2023-03-25 16:35:55 -0300

    update 1

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "./index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
